1429d4b0430c0c2cda7ca9dc891c55b867823549
Default <default>
2018-10-24 16:32:16 +0530

    CanaryTest, created test file.

---
+++ GithubMVITests/CanaryTest.swift
@@ -0,0 +1,15 @@
+//
+// Created by Dinesh IIINC on 24/10/18.
+// Copyright (c) 2018 kite.work. All rights reserved.
+//
+
+import Foundation
+import XCTest
+@testable import GithubMVI
+
+class CanaryTest: XCTestCase {
+
+  func canaryTest() {
+    XCTAssertEqual(true, true)
+  }
+}
